7953b039435f9187c6defe95d39d0b50c4199048
Shafayathub <shafayat.ms@gamilcom>
2022-11-07 21:23:52 +0600

    login page and register page  added

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,97 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import login from '../../images/Developer-activity.gif';
 
 const Register = () => {
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  };
   return (
-    <div>
-      <h3>Registration is going on....</h3>
-    </div>
+    <form onSubmit={handleSubmit} className="text-gray-600 body-font">
+      <div className="container px-5 py-8 mx-auto flex flex-wrap items-center">
+        <img className="mx-auto w-96" src={login} alt="" />
+
+        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-7 md:mt-0">
+          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+            Register
+          </h2>
+          <div className="relative mb-4">
+            <label htmlFor="full-name" class="leading-7 text-sm text-gray-600">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="full-name"
+              name="full-name"
+              class="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              ref={emailRef}
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              password
+            </label>
+            <input
+              ref={passwordRef}
+              type="password"
+              id="password"
+              name="password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
+            />
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Confirm password
+            </label>
+            <input
+              ref={passwordRef}
+              type="password"
+              id="password"
+              name="password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              required
+            />
+          </div>
+          <div className="relative mb-4">
+            <p>
+              <small>
+                Already have an account?
+                <Link to="/login" className="underline text-blue-600 ml-1">
+                  Sign in
+                </Link>
+              </small>
+            </p>
+          </div>
+
+          <input
+            type="submit"
+            value="Sign in"
+            className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
+          />
+          <p className="text-xs text-gray-500 mt-3">
+            <small>We care about your privacy.</small>
+          </p>
+        </div>
+      </div>
+    </form>
   );
 };
 

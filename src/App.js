f38bfa8e641dd0a140eabe27c1ba2bd73376ac44
Shafayathub <shafayat.ms@gamilcom>
2022-11-08 14:17:49 +0600

    demo site is rady

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './pages/Home/Home/Home';
 import ServiceDetail from './pages/Home/Services/ServiceDetail/ServiceDetail';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import Checkout from './pages/Checkout/Checkout';
+import RequireAuth from './pages/Login/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -21,6 +23,14 @@ function App() {
         <Route path="/about" element={<About></About>} />
         <Route path="/login" element={<Login></Login>} />
         <Route path="/register" element={<Register></Register>} />
+        <Route
+          path="/checkout"
+          element={
+            <RequireAuth>
+              <Checkout></Checkout>
+            </RequireAuth>
+          }
+        ></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/pages/Home/Services/ServiceDetail/ServiceDetail.js
+++ src/pages/Home/Services/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
   const { serviceID } = useParams();
   return (
-    <div>
+    <div className="mt-10 text-center">
       <h3>Service Details: {serviceID}</h3>
+      <Link to="/checkout">
+        <button className="p-2 bg-blue-500 rounded-md border-2 hover:bg-blue-600 text-white font-medium my-5">
+          Proceed Checkout
+        </button>
+      </Link>
     </div>
   );
 };

d64a125db15bd7805b7b5166ea03890ccc3ed7e4
Shafayathub <shafayat.ms@gamilcom>
2022-11-08 21:36:40 +0600

    social login added

--- src/pages/Login/SocialLogin/SocialLogin.js
+++ src/pages/Login/SocialLogin/SocialLogin.js
@@ -4,13 +4,13 @@ import fb from '../../../images/fb_icon.png';
 
 const SocialLogin = () => {
   return (
-    <div className="w-full mx-auto mt-3">
-      <div className="flex items-center justify-center space-x-3">
-        <div className="bg-blue-600 h-1 w-1/3 rounded"></div>
-        <p>Or</p>
-        <div className="bg-blue-600 h-1 w-1/3 rounded"></div>
+    <div className="w-full mx-auto mt-5">
+      <div className="flex items-center justify-evenly">
+        <div className="bg-blue-700 h-1 w-1/3 rounded"></div>
+        <p className="text-gray-900 font-semibold">Or</p>
+        <div className="bg-blue-700 h-1 w-1/3 rounded"></div>
       </div>
-      <div className="w-full flex justify-center space-x-11">
+      <div className="w-full flex justify-evenly space-x-11 mt-5">
         <button className="w-1/3 bg-gray-300 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
           <img src={google} alt="Sign in with Google" className="w-8 mx-auto" />
         </button>
